c51874148cbb753938169d2423706ad124a7012b
Arvind Gautam <iitarvindgautam@gmail.com>
2025-02-26 16:38:29 +0530

    Updated project files

--- Basic_Problems/print_N_to_1.java
+++ Basic_Problems/print_N_to_1.java
@@ -1,11 +1,15 @@
-public class Print_N_to_1 {
+package Basic_Problems;
+
+public class print_N_to_1 {
+
 
     public static void printNum(int n){
         if(n==1){
             System.out.print(1+" ");
+            System.out.println();
             return;
         }
-        System.out.println(n+" ");
+        System.out.print(n+" ");
         printNum(n-1);
     }
     public static void main(String[] args) {
