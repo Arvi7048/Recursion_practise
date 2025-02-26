9f9962d4128fc8c38cb84055b5e87cf8663c0983
Arvind Gautam <iitarvindgautam@gmail.com>
2025-02-26 16:32:40 +0530

    Updated project files

--- Print_N_to_1.java
+++ Print_N_to_1.java
@@ -1,13 +1,15 @@
-public class PrintNto1 {
+public class Print_N_to_1 {
+
     public static void printNum(int n){
         if(n==1){
-            System.out.println(1);
+            System.out.print(1+" ");
             return;
         }
-        System.out.println(n);
+        System.out.println(n+" ");
         printNum(n-1);
     }
     public static void main(String[] args) {
         printNum(5);
     }
 }
+
